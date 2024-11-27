58a00e823c89d0bc0bf94fd53c29bab6e32649e8
Ali Unwala <>
2024-11-26 22:40:42 -0600

    Adding in show/hide for password input field

--- app/frontend/reusable-components/flow-layout/flow-layout.tsx
+++ app/frontend/reusable-components/flow-layout/flow-layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../button/button';
 interface Props {
@@ -6,18 +6,23 @@ interface Props {
 }
 
 export function FlowLayout({ children }: Props) {
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('wealthFrontToken') != null ? true : false);
   function handleLogout() {
     localStorage.removeItem('wealthFrontToken');
-    // nav('/create-account');
   }
+  useEffect(() => {
+    setLoggedIn(localStorage.getItem('wealthFrontToken') != null ? true : false);
+  }, []);
   return (
     <div className="h-full mt-5 max-w-[1000px] m-4">
       <div className="w-full text-right">
-        <Link onClick={handleLogout} to="/create-account" reloadDocument>
-          <Button colorText="rgb(72, 64, 187)" colorBG="white" border={true}>
-            Logout
-          </Button>
-        </Link>
+        {loggedIn && (
+          <Link onClick={handleLogout} to="/create-account" reloadDocument>
+            <Button colorText="rgb(72, 64, 187)" colorBG="white" border={true}>
+              Logout
+            </Button>
+          </Link>
+        )}
       </div>
       <div className="flex items-center justify-center">{children}</div>
     </div>
--- app/frontend/routes/account/password-strength-bar.test.tsx
+++ app/frontend/routes/account/password-strength-bar.test.tsx
@@ -23,7 +23,6 @@ describe('CreateAccount subcomponent: password-strength-bar', () => {
   });
 
   test('1) Type "11111111111111111111" into the password field 2) confirm we see the "weak" message', async () => {
-    // https://stackoverflow.com/questions/70029935/react-router-v6-usenavigate-may-be-used-only-in-the-context-of-a-router-co
     render(
       <Router>
         <CreateAccount />
@@ -38,7 +37,6 @@ describe('CreateAccount subcomponent: password-strength-bar', () => {
   });
 
   test('1) Type "111111111111111112345" into the password field 2) confirm we see the "okay" message', async () => {
-    // https://stackoverflow.com/questions/70029935/react-router-v6-usenavigate-may-be-used-only-in-the-context-of-a-router-co
     render(
       <Router>
         <CreateAccount />
@@ -53,7 +51,6 @@ describe('CreateAccount subcomponent: password-strength-bar', () => {
   });
 
   test('1) Type "11111111111111111234567" into the password field 2) confirm we see the "good" message', async () => {
-    // https://stackoverflow.com/questions/70029935/react-router-v6-usenavigate-may-be-used-only-in-the-context-of-a-router-co
     render(
       <Router>
         <CreateAccount />
@@ -68,7 +65,6 @@ describe('CreateAccount subcomponent: password-strength-bar', () => {
   });
 
   test('1) Type "11111111111111111234567abc" into the password field 2) confirm we see the "strong" message', async () => {
-    // https://stackoverflow.com/questions/70029935/react-router-v6-usenavigate-may-be-used-only-in-the-context-of-a-router-co
     render(
       <Router>
         <CreateAccount />
@@ -81,25 +77,4 @@ describe('CreateAccount subcomponent: password-strength-bar', () => {
       expect(screen.getByText('strong')).toBeInTheDocument();
     });
   });
-
-  //   test('1) Render page 2) Add "bad" to user/pass fields + submit 3) Confirm we see error messages', async () => {
-  //     render(
-  //       <Router>
-  //         <CreateAccount />
-  //       </Router>
-  //     );
-
-  //     const usernameText = screen.getByPlaceholderText('Username');
-  //     const passwordText = screen.getByPlaceholderText('Password');
-  //     const formSubmitBtn = screen.getByRole('button', { name: /Create Account/ });
-
-  //     fireEvent.change(usernameText, { target: { value: 'bad' } });
-  //     fireEvent.change(passwordText, { target: { value: 'bad' } });
-  //     fireEvent.click(formSubmitBtn);
-  //     // https://testing-library.com/docs/guide-disappearance/
-  //     await waitFor(() => {
-  //       expect(screen.getByText(/String must contain at least 10 character\(s\)/)).toBeInTheDocument();
-  //       expect(screen.getByText(/String must contain at least 20 character\(s\)/)).toBeInTheDocument();
-  //     });
-  //   });
 });
